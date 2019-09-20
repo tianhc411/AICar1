2951420d7f1d57c669c305b543d76fa5e0a4784e
tianhc411 <tianhc411@163.com>
2019-09-20 15:23:49 +0800

    实现Android客户端与C语言服务器端的数据传输

--- app/src/main/java/com/example/aicar/MainActivity.java
+++ app/src/main/java/com/example/aicar/MainActivity.java
@@ -16,18 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final ImageView a = findViewById(R.id.Y0);
-//        SocketThread socketThread = new SocketThread();
-
-//        try {
-//            Socket socket = serverClient.getLink();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
         a.setOnTouchListener(new View.OnTouchListener() {
             float lastRawX, lastRawY;
             float m1=280,m2=555;//摇杆起始点
+            byte p1 = '0' ,p2 = '0';
+            Client c= new Client(); //新建客户端对象
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
+            public boolean onTouch(final View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         lastRawX = event.getRawX();
@@ -37,13 +32,28 @@ public class MainActivity extends AppCompatActivity {
                         float dx = (event.getRawX() - lastRawX-80);
                         float dy = (event.getRawY() - lastRawY-245);
                         float i=event.getRawX()+dx,j=event.getRawY()+dy;
+//                       byte distance =(byte)Math.sqrt(Math.pow(i-280,2)+Math.pow(j-555,2));
                        if (Math.sqrt(Math.pow(i-280,2)+Math.pow(j-555,2))<=110) {
                            v.setX(i);
                            v.setY(j);
-                           double number = Math.atan2((i- 280), (j - 555));
-                           double angle = number*(180/Math.PI);
-                           SocketThread socketThread = new SocketThread(angle);
-                           socketThread.start();
+//                           byte cosAngle = (byte) ((i-280)/distance);
+//                           byte rad = (byte) Math.acos(cosAngle);
+//                           if (j < 555) {
+//                               rad =(byte) -rad;
+//                           }
+                           if (i>280)
+                               p2='2';
+                           else if (i<280)
+                               p2='1';
+                           else p2='0';
+                           p1='1';
+                           new Thread(new Runnable() {   //新的线程来执行客户端
+                               @Override
+                               public void run() {    //运行run方法
+                                   c.send(p1, p2);   //调用send方法
+                                   p1=p2=0;
+                               }
+                           }).start();
                        }
                         lastRawX = event.getRawX();
                         lastRawY = event.getRawY();
